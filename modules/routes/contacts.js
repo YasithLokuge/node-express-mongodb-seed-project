8f18c47323449880e12c8c114f808f4a875fdbe9
guptam2 <madhur.gupta@theaa.com>
2015-11-20 15:23:37 +0000

    changed the contact dto

--- modules/routes/contacts.js
+++ modules/routes/contacts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var logger= require('../utils/logger.js');
 var mongoose = require('mongoose');
 
-var db = mongoose.connect('mongodb://127.0.0.1:27017/customer');
+var customerDB = mongoose.connect('mongodb://127.0.0.1:27017/customer');
 
 var contactSchema = mongoose.Schema({
 	'first_name': 'string',
@@ -13,7 +13,7 @@ var contactSchema = mongoose.Schema({
 	'address_line_2': 'string',
 	'county' : 'string',
 	'postcode':  'string'
-});
+},{collection:'contacts'});
 
 var Contact = mongoose.model('Contact', contactSchema);
 
@@ -22,14 +22,33 @@ router.get('/', function(req, res){
 	
 	logger.info('Retrieving data from mongodb');
 	
+	var contactDTO = [];
+	
 	Contact.find(function(err, contacts){
 		if(err){
 			res.json({"error":true});
 		}else{
-			res.json(contacts);
+			
+			for(var i=0; i < contacts.length ; i++ ){
+				
+				var c = contacts[i];
+				
+				contactDTO.push(
+				{
+					"firstName": c.first_name,
+					"lastName":c.last_name,
+					"addressLine1":c.address_line_1,
+					"addressLine2":c.address_line_2,
+					"county":c.county,
+					"postcode":c.postcode,
+					"phone":c.phone
+				}
+				
+				);
+			}
+			res.json(contactDTO);
 		}	
 	});
-	
 });
 
 //POST request
@@ -39,19 +58,19 @@ router.post('/', function(req, res){
 	
 	var reqContact = req.body;
 	
-	var contact = new Contact({	
-		first_name:reqContact.first_name,
-		last_name:reqContact.last_name,
+	var c = new Contact({	
+		first_name:reqContact.firstName,
+		last_name:reqContact.lastName,
 		phone: reqContact.phone,
-		address_line_1: reqContact.address_line_1,
-		address_line_2: reqContact.address_line_2,
+		address_line_1: reqContact.addressLine1,
+		address_line_2: reqContact.addressLine2,
 		county: reqContact.county,
 		postcode: reqContact.postcode
 	});
 	
 	var resMsg = {"success":true};
 	
-	contact.save(function(err, c){
+	c.save(function(err, c){
 		if (err){
 			resMsg = {"error":true};
 			logger.error(err);
